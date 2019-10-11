ff714d878ec7e9137e5ccd040502303fcc0969aa
Leandro Silveira <leandrobhmgg@gmail.com>
2019-10-11 00:58:38 -0300

    console print

--- src/Quiz.Api/Views/Home/Scripts/Utils.js
+++ src/Quiz.Api/Views/Home/Scripts/Utils.js
@@ -19,3 +19,26 @@ jQuery(document).ready(function ($) {
     initPage();
 
 });
+
+$.ajaxSetup({
+    cache: false,
+    dataType: "JSON",
+    contentType: "application/json; charset=utf-8",
+    beforeSend: function (request, settings) {
+        var data = JSON.parse(settings.data);
+        console.info("--- GRAPHQL QUERY-----------------\n\n " + data.query + " -------------------------\n\n");
+    },
+    complete: function (e, status) {
+        if (status == 'success') {
+
+            try {
+               
+                    var responseText = JSON.parse(e.responseText);
+                    console.info("Dados -------------------\n")
+                    console.info(responseText);
+                
+            } catch (err) {
+            }
+        }
+    }
+});
